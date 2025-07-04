74bef27f28239004d953b3f087183f0ef58e4495
hyuns000 <hsss6349@hs.ac.kr>
2025-07-04 18:53:52 +0900

    리액트 네이티브 네비게이션 설치 및 세팅, 네비게이션 사용

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+//2강 HomeScreen 컴포넌트 생성
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text Style={{fontSize: 40, fontWeight: "bold"}}>메인 화면</Text>
+    </View>
+  );
+}
+
+const DetailScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text Style={{fontSize: 40, fontWeight: "bold"}}>상세보기 화면</Text>
+    </View>
+  );
+}
+
+//2강 상단좌측에 HOME
+const StackActions = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <StackActions.Navigator initialRouteName='Home'>
+      <StackActions.Screen name="Home" component={HomeScreen} />
+      <StackActions.Screen name="Details" component={DetailScreen} />
+    </StackActions.Navigator>
+   </NavigationContainer>
   );
 }
 
